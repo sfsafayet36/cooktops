91be6b619565f15aa4a99fb195765845c31d6103
sf.safayet36@gmail.com <sf.safayet36@gmail.com>
2023-09-10 16:22:13 +0600

    cooktops

---
+++ main.js
@@ -0,0 +1,33 @@
+let images = document.getElementById('images')
+let pos;
+function addrun() {
+    pos = 0;
+    let setint = setInterval(function () {
+
+        pos++
+        images.style.right = pos + 'px';
+        images.style.transition = '10ms ease'
+
+        if (pos == 1225) {
+            clearInterval(setint)
+            addreverse()
+        }
+
+
+    }, 50)
+}
+addrun()
+
+function addreverse() {
+    let setint2 = setInterval(function () {
+        pos--
+
+        images.style.right = pos + 'px'
+
+        if (pos == 0) {
+            clearInterval(setint2)
+            addrun()
+        }
+    }, 50)
+}
+
